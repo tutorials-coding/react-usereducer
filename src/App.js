7e9f6a7048e558d60bbfe2781caedf5687d7d587
devmagazinepodcast <devmagazinepodcast@gmail.com>
2020-07-04 19:03:41 +0300

    Add reducer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { Home } from './screens'
 import { Counter } from './screens'
 
+const initialCount = 0
+
 export default function App() {
   return (
     <div>
       {/* <Home /> */}
-      <Counter />
+      <Counter initialCount={initialCount} />
     </div>
   )
 }
--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import Button from 'react-bootstrap/Button'
 
 import './Counter.css'
 
-export function Counter() {
-  function increment() {}
+function initState(initialCount) {
+  return {
+    count: initialCount,
+  }
+}
 
-  function decrement() {}
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 }
+    case 'decrement':
+      return { count: state.count - 1 }
+    case 'reset':
+      return initState(action.payload)
+    default:
+      throw new Error('Unknown action')
+  }
+}
 
-  function reset() {}
+export function Counter({ initialCount }) {
+  const [state, dispatch] = useReducer(reducer, initialCount, initState)
+
+  function increment() {
+    dispatch({ type: 'increment' })
+  }
+
+  function decrement() {
+    dispatch({ type: 'decrement' })
+  }
+
+  function reset() {
+    dispatch({ type: 'reset', payload: initialCount })
+  }
 
   return (
     <div className="counter__main-container">
-      <span className="counter__counter-value">0</span>
+      <span className="counter__counter-value">{state.count}</span>
       <div className="d-flex flex-column counter__buttons-container">
         <Button variant="primary" onClick={increment}>
           Increment
