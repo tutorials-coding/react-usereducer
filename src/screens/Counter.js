fed5b9a56a53e6843ca62ee319237bcf354d4d97
devmagazinepodcast <devmagazinepodcast@gmail.com>
2020-07-04 18:48:45 +0300

    Add handlers tempaltes

--- src/screens/Counter.js
+++ src/screens/Counter.js
@@ -4,13 +4,25 @@ import Button from 'react-bootstrap/Button'
 import './Counter.css'
 
 export function Counter() {
+  function increment() {}
+
+  function decrement() {}
+
+  function reset() {}
+
   return (
     <div className="counter__main-container">
       <span className="counter__counter-value">0</span>
       <div className="d-flex flex-column counter__buttons-container">
-        <Button variant="primary">Increment</Button>
-        <Button variant="primary">Decrement</Button>
-        <Button variant="primary">Reset</Button>
+        <Button variant="primary" onClick={increment}>
+          Increment
+        </Button>
+        <Button variant="primary" onClick={decrement}>
+          Decrement
+        </Button>
+        <Button variant="primary" onClick={reset}>
+          Reset
+        </Button>
       </div>
     </div>
   )
